b16089d30ef9c0c8171662c780d4cf6c03f1dc26
Aykut Sezgin <aykut-sezgin@hotmail.com>
2019-03-26 11:25:18 +0300

    animations added.

---
+++ assets/js/animation.js
@@ -0,0 +1,95 @@
+window.sr = ScrollReveal();
+sr.reveal('.headAnimation', {
+    duration: 2500,
+    scale: .75
+});
+
+sr.reveal('.featuresAnimation', {
+    duration: 2500,
+    origin: 'top',
+    distance: '60px'
+});
+
+sr.reveal('.historyLeftAnimation', {
+    distance: '100px',
+    duration: 2500,
+    origin: 'left',
+    viewFactor: 0.2
+});
+
+sr.reveal('.historyRightAnimation', {
+    distance: '100px',
+    duration: 2500,
+    origin: 'right',
+    viewFactor: 0.2
+});
+
+sr.reveal('.teamAnimation-1', {
+    distance: '20px',
+    duration: 2500,
+    origin: 'left',
+    viewFactor: 0.2
+});
+
+sr.reveal('.teamAnimation-2', {
+    distance: '20px',
+    duration: 2500,
+    origin: 'left',
+    viewFactor: 0.2,
+    delay: 300
+});
+
+sr.reveal('.teamAnimation-3', {
+    distance: '20px',
+    duration: 2500,
+    origin: 'left',
+    viewFactor: 0.2,
+    delay: 600,
+});
+
+sr.reveal('.teamAnimation-4', {
+    distance: '20px',
+    duration: 2500,
+    origin: 'left',
+    viewFactor: 0.2,
+    delay: 900
+});
+
+sr.reveal('.whatWeDoAnimation', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '50px'
+});
+
+sr.reveal('.caseAnimation', {
+    distance: '80px',
+    duration: 2500,
+    origin: 'left',
+    viewFactor: 0.2
+});
+
+sr.reveal('.testimonialAnimation', {
+    distance: '50px',
+    duration: 2000,
+    origin: 'bottom',
+    viewFactor: 0.2
+});
+
+sr.reveal('.contactAnimation-1', {
+    distance: '60px',
+    duration: 2500,
+    origin: 'left',
+    viewFactor: 0.2
+});
+
+sr.reveal('.contactAnimation-2', {
+    distance: '60px',
+    duration: 2500,
+    origin: 'right',
+    viewFactor: 0.2
+});
+
+sr.reveal('.portfolioAnimation', {
+    duration: 2500,
+    scale: 1.1
+});
